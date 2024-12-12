377e7eb43386d65ed9537dc518ae013c14facff7
sagarGongit <sagarbhagwat1348.sb@gmail.com>
2024-12-12 15:41:44 +0530

    update

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const Mongo_Connection = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/bewakoof");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("database connected");
   } catch (error) {
     console.log(error);
