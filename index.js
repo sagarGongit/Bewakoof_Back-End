152af4a12dfed6609770fe84ec31b9f57fbc6c5d
sagarGongit <sagarbhagwat1348.sb@gmail.com>
2024-12-13 23:00:04 +0530

    update

--- index.js
+++ index.js
@@ -32,6 +32,45 @@ app.get("/", (req, res) => {
   });
 });
 
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: process.env.REDIRECT_URI,
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return cb(err, user);
+      });
+    }
+  )
+);
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
+app.use(
+  session({ secret: "secretKey", resave: false, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile"] })
+);
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function (req, res) {
+    res.redirect("/");
+  }
+);
+
 app.listen(PORT, () => {
   Mongo_Connection();
   console.log(`your server is live on port ${PORT}`);
